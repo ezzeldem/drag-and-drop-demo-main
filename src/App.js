12b556366525a6ba4bfdccf12558fc81166d1bbd
ezzeldem <ezzelden06@gmail.com>
2022-11-17 01:28:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SortableList from "./components/SortableList";
+
+const App = () => {
+  return (
+    <div className='container'>
+      <SortableList />
+    </div>
+  );
+};
+
+export default App;
